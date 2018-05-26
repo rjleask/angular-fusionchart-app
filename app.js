203f98a7dc87e7e604ebf4dc42118f760acd3160
Jamie <rjamie.leask@gmail.com>
2018-05-26 00:54:05 -0400

    added two data formatting functions

--- app.js
+++ app.js
@@ -9,6 +9,40 @@ myApp.controller("mainController", [
       )
       .success(function(response) {
         console.log(response);
+        let data = response.feed.entry;
+        let closeAmountObj = {};
+
+        for (let i = 0; i < data.length; i++) {
+          let tempArr = [];
+          let tempObj = {};
+
+          formatCloseAmount(
+            data[i].gsx$closeamount.$t,
+            data[i].gsx$airlinename.$t
+          );
+        }
+        // calls checkObj to update closeAmountObj close $amounts
+        function formatCloseAmount(close, airline) {
+          // tempNum replaces all the dollar sign characters and converts it to float num
+          let tempNum = parseFloat(close.replace(/\$|,/g, ""));
+          let tempAirline = closeAmountObj;
+          if (tempNum !== NaN && tempNum > 0) {
+            closeCheckObj(tempAirline, airline, tempNum);
+          }
+        }
+        // checks to see if objProp exists, if it does increment
+        // else make sure it's a normal character and initialize
+        function closeCheckObj(data, property, num) {
+          if (data.hasOwnProperty(property)) {
+            data[property] += num;
+          } else {
+            // checks for alphanumeric characters only
+            let filter = /^[a-zA-Z]/g.test(property);
+            if (filter) {
+              data[property] = num;
+            }
+          }
+        }
       });
   }
 ]);
